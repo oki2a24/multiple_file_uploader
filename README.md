cc8ee1b3c61b03f2cf2fe585a0b7400874ffd305
oki2a24 <oki2a24@gmail.com>
2018-09-13 08:26:14 +0900

    動くようにする

---
+++ README.md
@@ -0,0 +1,18 @@
+## はじめに
+- 複数ファイルをサーバにアップロードします。
+- ファイルの選択は、ドラッグすることでも可能です。
+- CodeIgniter3 を使用しています。
+
+## 使い方
+http://localhost/multiple_file_uploader/index.php/filehandle
+
+ファイルアップロードが成功すると、uploads ディレクトリにファイルが置かれます。
+
+## 設定
+下記のように URL を設定しています。必要に応じて変更してください。
+$config['base_url'] = 'http://localhost/multiple_file_uploader';
+
+## おわりに
+このリポジトリは、次をフォークしています。感謝いたします。
+
+- [teru01/multiple_file_uploader: sample project for uploading multiple files with codeigniter.](https://github.com/teru01/multiple_file_uploader)
--- application/views/upload.php
+++ application/views/upload.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Upload Form</title>
-<link rel="stylesheet" type="text/css" href="/css/style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
 </head>
 <body>
     <form action="filehandle/upload" method='post' enctype='multipart/form-data'> 
@@ -20,6 +20,6 @@
         integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="/js/uploadFile.js"></script>
+<script src="<?php echo base_url('js/uploadFile.js'); ?>"></script>
 </body>
 </html>
